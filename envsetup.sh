fbf3f7faf2a38aee03a28cdd05eb74982c67ee07
Abhinav Jhanwar <ajhanwar97@gmail.com>
2020-04-25 21:15:46 +0530

    script: Change username and hostname

--- envsetup.sh
+++ envsetup.sh
@@ -6,5 +6,5 @@ export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date -u +%Y.%m.%d.
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
 export PATH="$PWD/script/bin:$PATH"
-export BUILD_USERNAME=grapheneos
-export BUILD_HOSTNAME=grapheneos
+export BUILD_USERNAME=user
+export BUILD_HOSTNAME=host
